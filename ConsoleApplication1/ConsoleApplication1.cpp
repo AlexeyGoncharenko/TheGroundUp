91ce7778ce719914769f4d383b0bcdaef7110a8d
AlexeyGoncharenko <alexey.goncharenko85@gmail.com>
2017-10-13 14:23:37 +0300

    Add setlocale to resolve compatible app. WinOS lang input changed ENG. VS chaged codepage of source files to UTF-8.

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,13 +1,21 @@
-// ConsoleApplication1.cpp : Defines the entry point for the console application.
+﻿// ConsoleApplication1.cpp : Defines the entry point for the console application.
 #include "stdafx.h"
 #include <string>
-#include <stdlib.h>
+#include <Windows.h>
 
 int main(){
-	std::string answer = "This is ";
+	setlocale(LC_ALL, "ru_RU.UTF-8");
+	SetConsoleCP(CP_UTF8);
+	SetConsoleOutputCP(CP_UTF8);
+	// Инициализация программы 
+	std::string  answer = "Инициализация приложения... ";
+	std::cout << answer.c_str() << std::endl;
+
+	std::cout << "Compiler's version: " << __cplusplus << std::endl;
+
+	answer = "This is ";
 	answer.append((sizeof(char) == sizeof('c')) ? "C++ program!\n" : "C program!\n");
 	std::cout << answer << std::endl;
-	std::cout << "Compiler's version: " << __cplusplus << std::endl;
+		
 	return EXIT_SUCCESS;
 }
-
